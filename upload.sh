9e287b687d0dc4803a133fbc1d91531ab8bf2241
Your Name <you@example.com>
2020-11-02 19:54:22 +0000

    All Updated

--- upload.sh
+++ upload.sh
@@ -3,4 +3,6 @@ git config --global user.name "Your Name"
 git config --global user.email "you@example.com"
 git add *
 git commit -m "All Updated"
-git push -u "master"
+git branch -M main
+git remote add origin https://github.com/Kamakepar2029/kamakepar-site-new.git
+git push -u origin main
